ea3ce8d22f066373d67c76d3a131d1bcdba41e38
Leonardo-1998 <leonardo.agst98@gmail.com>
2026-01-19 13:24:06 +0700

    First Setup Backend

---
+++ BackEnd/config/Migrate.js
@@ -0,0 +1,96 @@
+const pool = require("./Config");
+
+console.log("===== Migrate =====");
+
+const migrate = async () => {
+  try {
+    // ===== Drop Table =====
+    const queryDropTable = `
+        DROP TABLE IF EXISTS "Users", "Task_Lists", "Tasks", "Collaborators"
+    `;
+
+    const { rows: dropTable } = await pool.query(queryDropTable);
+
+    if (dropTable) {
+      console.log("Berhasil melakukan drop table.");
+    }
+    // ===== ===== ===== =====
+
+    //===== Create Users Table =====
+    const queryCreateTableUsers = `
+        CREATE TABLE IF NOT EXISTS "Users" (
+        id SERIAL PRIMARY KEY,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL
+        )
+    `;
+
+    const { rows: createTableUsers } = await pool.query(queryCreateTableUsers);
+
+    if (createTableUsers) {
+      console.log("Berhasil membuat table Users");
+    }
+
+    //===== ===== ===== =====
+
+    //===== Create Table Task_Lists =====
+    const queryCreateTableTaskLists = `
+        CREATE TABLE IF NOT EXISTS "Task_Lists" (
+        id SERIAL PRIMARY KEY,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL
+        )
+    `;
+
+    const { rows: createTableTaskLists } = await pool.query(
+      queryCreateTableTaskLists,
+    );
+
+    if (createTableTaskLists) {
+      console.log("Berhasil membuat table Task_Lists");
+    }
+
+    //===== ===== ===== =====
+
+    //===== Create Tasks Table =====
+    const queryCreateTableTasks = `
+        CREATE TABLE IF NOT EXISTS "Tasks" (
+        id SERIAL PRIMARY KEY,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL
+        )
+    `;
+
+    const { rows: createTableTasks } = await pool.query(queryCreateTableTasks);
+
+    if (createTableTasks) {
+      console.log("Berhasil membuat table Tasks");
+    }
+
+    //===== ===== ===== =====
+
+    //===== Create Collaborators Table =====
+    const queryCreateTableCollaborators = `
+        CREATE TABLE IF NOT EXISTS "Collaborators" (
+        id SERIAL PRIMARY KEY,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL
+        )
+    `;
+
+    const { rows: createTableCollaborators } = await pool.query(
+      queryCreateTableCollaborators,
+    );
+
+    if (createTableCollaborators) {
+      console.log("Berhasil membuat table Collaborators");
+    }
+
+    //===== ===== ===== =====
+  } catch (error) {
+    console.log("===== Error =====");
+    console.log("Terjadi Error pada saat migrating.");
+  }
+};
+
+migrate();
---
+++ BackEnd/model/Class.js
@@ -0,0 +1,34 @@
+class User {
+  constructor(email, password) {
+    this.email = email;
+    this.password = password;
+  }
+}
+
+class TaskList {
+  constructor(email, password) {
+    this.email = email;
+    this.password = password;
+  }
+}
+
+class Task {
+  constructor(email, password) {
+    this.email = email;
+    this.password = password;
+  }
+}
+
+class Collaborator {
+  constructor(email, password) {
+    this.email = email;
+    this.password = password;
+  }
+}
+
+module.exports = {
+  User,
+  TaskList,
+  Task,
+  Collaborator,
+};
