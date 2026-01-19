9834d665e3c26d1059683871ad2e9736a9aa48ad
Leonardo-1998 <leonardo.agst98@gmail.com>
2026-01-19 15:09:23 +0700

    Feat : Membuat fitur register dan login, serta menambahkan errorHandler

---
+++ BackEnd/controller/UserController.js
@@ -0,0 +1,107 @@
+const pool = require("../config/Config");
+const { hashPassword, comparePassword } = require("../utils/Bcryptjs");
+const ResponseError = require("../utils/ResponseError");
+
+class UserController {
+  static async registerUser(req, res, next) {
+    try {
+      const { email, password } = req.body;
+
+      const data = await UserController.getUserByEmail(email);
+      if (data) {
+        const error = new Error("Email sudah digunakan");
+        error.statusCode = 400;
+        throw error;
+      }
+
+      const hashedPassword = await hashPassword(password);
+
+      const query = `
+            INSERT INTO "Users" (email, password) 
+            VALUES ($1, $2)
+            RETURNing email
+        `;
+
+      const values = [email, hashedPassword];
+
+      const { rows: userEmail } = await pool.query(query, values);
+
+      res.status(200).json({
+        statusCode: 200,
+        message: userEmail,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async loginUser(req, res, next) {
+    try {
+      const { email, password } = req.body;
+
+      const data = await UserController.getUserByEmail(email);
+
+      if (!data) {
+        const error = new Error("Email atau Password tidak sesuai!");
+        error.statusCode = 400;
+        throw error;
+      }
+
+      const samePassword = await comparePassword(password, data.password);
+
+      if (!samePassword) {
+        const error = new Error("Email atau Password tidak sesuai!");
+        error.statusCode = 400;
+        throw error;
+      }
+
+      res.status(200).json({
+        statusCode: 200,
+        message: { email: data.email },
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async getAllUsers(req, res, next) {
+    try {
+      const query = `
+            SELECT * FROM "Users"
+        `;
+
+      const { rows: allUsersData } = await pool.query(query);
+
+      res.status(200).json({
+        statusCode: 200,
+        message: allUsersData,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async getUserByEmail(email) {
+    try {
+      const query = `
+            SELECT * FROM "Users"
+            WHERE email = $1
+        `;
+
+      const values = [email];
+      const { rows: userData } = await pool.query(query, values);
+      return userData[0];
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async X(req, res, next) {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+module.exports = UserController;
